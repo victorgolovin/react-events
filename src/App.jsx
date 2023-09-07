cb6856c96eb531d99c11ca5c27780b6bfcbee46f
unknown <rinoally@gmail.com>
2023-09-07 14:48:01 +0300

    add commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
+  const handleClick = () => {
+    alert('clicked')
+  }
+
+  const handleMouseOver = () => {
+    console.log('mouse over event')
+  }
+
+  const handleLinkClick = (e) => {
+    e.preventDefault();
+    console.log('link clicked')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <a 
+      onClick={handleLinkClick}
+      href="https://another-page.ru">Ссылка</a>
+      <button 
+      onClick={handleClick}
+      onMouseOver={handleMouseOver}
+      >
+      Push me
+      </button>
+    </div>
+    
   )
 }
 
